cf112a9c69f80d470c2abdcfeb83a89deb81e60b
Duchai263 <duchai263@gmail.com>
2024-10-15 21:26:44 +0700

    add histogram

--- app.py
+++ app.py
@@ -1,26 +1,40 @@
 from flask import Flask, render_template, request, send_file
 import os
+import sys
+import shutil
+import cv2
+import histogram
+import numpy as np
 
 app = Flask(__name__)
 
-imgs_path = r".\static\data\seg"
+dataset_path = r".\static\data\seg"
+histogram_path = r".\static\data\histograms"
 
+if (not os.path.exists(dataset_path)):
+    sys.exit("NO IMAGES DATASET")
+
+def calcHists():
+    for scene in os.listdir(dataset_path):
+        scene_imgs = os.path.join(dataset_path,scene)
+        for image_name in os.listdir(scene_imgs):
+            img_path = os.path.join(scene_imgs,image_name)
+            
+            hist = histogram.calcHist(img_path)
+            # print(os.path.splitext(image_name))
+            np.save(os.path.join(histogram_path,os.path.splitext(image_name)[0]),hist)
+            # exit()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         image = request.files['file']
-        image = []
-        for scene in os.listdir(imgs_path):
-            # print(scene)
-            scene_imgs = os.path.join(imgs_path,scene)
-            for img in os.listdir(scene_imgs):
-                img_path = os.path.join(scene_imgs,img)
-                image.append(img_path)
-                break
-            # image.append()
         return render_template('index.html',result=image)
     return render_template('index.html')
 
 if __name__ == '__main__':
+    if (not os.path.exists(histogram_path)):
+        os.makedirs(histogram_path)
+        calcHists()
+
     app.run(port=9000)
--- histogram.py
+++ histogram.py
@@ -1,5 +1,20 @@
 import os
 import cv2
+import numpy as np
 
-def calchist(img):
-    
+def calcHist(img_path):
+    img = cv2.imread(img_path)
+    # img = cv2.split(img)
+    if img is None:
+        print('Could not open or find the image')
+        return
+
+    b_hist = cv2.calcHist([img], [0], None, [256], [0,256])
+    g_hist = cv2.calcHist([img], [1], None, [256], [0,256])
+    r_hist = cv2.calcHist([img], [2], None, [256], [0,256])
+
+    return np.array([b_hist,g_hist,r_hist])
+
+def distanceEuclidian(hist1,hist2):
+    return np.linalg.norm(hist1 - hist2)
+
